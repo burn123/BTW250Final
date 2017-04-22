c652d2bad83e3fb54c2a2680bde10064ba3a5933
Cody <burningwolf96@gmail.com>
2017-04-22 11:51:29 -0500

    Add gulp for javascript

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,33 @@
 var gulp         = require('gulp'),
     postcss      = require('gulp-postcss'),
-    sourcemaps   = require('gulp-sourcemaps'),
     autoprefixer = require('gulp-autoprefixer'),
-    sass         = require('gulp-sass');
+    sass         = require('gulp-sass'),
+    concat       = require('gulp-concat'),
+    uglify       = require('gulp-uglify'),
+    rename       = require('gulp-rename');
 
-var input = ['./css/*.css', './css/*.scss'];
+var css_input = ['./css/*.css', './css/*.scss'],
+    js_input = 'js/*.js';
 
 gulp.task('autoprefixer', function () {
-    gulp.src(input)
+    gulp.src(css_input)
         // Error log to keep session going when scss contains error
         .pipe(sass().on('error', sass.logError)) 
         .pipe(autoprefixer('last 2 version'))
         .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('default', ['autoprefixer'], function() {
-        gulp.watch(input, ['autoprefixer']);
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    gulp.src(js_input)
+        .pipe(concat('all.min.js'))
+        //.pipe(gulp.dest('dist'))
+        //.pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['autoprefixer', scripts], function() {
+        gulp.watch(css_input, ['autoprefixer']);
+        gulp.watch(js_input, ['scripts']);
 });
